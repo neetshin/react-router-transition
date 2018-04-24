d58c43bab2a5a66d47323c8edd14de9f532a2e35
Neetshin <neetshin@neetsh.in>
2018-04-24 21:42:43 +0530

    Add typescript definition files

---
+++ index.d.ts
@@ -0,0 +1,49 @@
+import * as React from 'react';
+
+interface RouteTransitionProps {
+  className?: string;
+  wrapperComponent?: boolean|JSX.Element|string;
+  atEnter: React.CSSProperties;
+  atActive: React.CSSProperties;
+  atLeave: React.CSSProperties;
+  didLeave?: (...args: any[]) => void;
+  mapStyles?: (...args: any[]) => void;
+  runOnMount?: boolean;
+}
+
+export class RouteTransition extends React.Component<RouteTransitionProps, {}> {}
+
+
+interface AnimatedRouteProps extends RouteTransitionProps {
+  component: React.ReactNode;
+  path: string;
+  exact?: boolean;
+}
+
+export class AnimatedRoute extends React.SFC<AnimatedRouteProps> {}
+
+
+interface AnimatedSwitchProps extends AnimatedRouteProps {
+  location?: {
+    key?: string;
+    pathname?: string;
+  }
+}
+
+export class AnimatedSwitch extends React.Component<AnimatedSwitchProps> {}
+
+
+interface SpringHelperConfig {
+  stiffness?: number;
+  damping?: number;
+  precision?: number;
+}
+
+interface OpaqueConfig {
+  val: number;
+  stiffness: number;
+  damping: number;
+  precision: number;
+}
+
+export declare function spring(val: number, config?: SpringHelperConfig): OpaqueConfig;
